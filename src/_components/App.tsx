4c0b6fefa02de3f29d9818a41326ce510f8d969b
Авилов Дмитрий Игоревич <avilov.dmitriy@wildberries.ru>
2020-11-06 23:33:43 +0300

    init

---
+++ src/_components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const CLASS_NAME = "App";
+
+function App() {
+  return (
+    <div className={CLASS_NAME}>
+      <header className={`${CLASS_NAME}__header`}>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
