8bf7617f4d1a962de8b4e59a3c24167442a329ca
Авилов Дмитрий Игоревич <avilov.dmitriy@wildberries.ru>
2020-11-09 02:39:59 +0300

    init

--- src/_components/App.tsx
+++ src/_components/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import './App.css';
+import classname from 'classnames/bind';
+import styles from './App.module.scss'
 
 const CLASS_NAME = "App";
+const cn = classname.bind(styles);
 
 function App() {
   return (
-    <div className={CLASS_NAME}>
-      <header className={`${CLASS_NAME}__header`}>
-      
+    <div className={cn(CLASS_NAME)}>
+      <header className={cn(`${CLASS_NAME}__header`)}>
+      1234
       </header>
     </div>
   );
