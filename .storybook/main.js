bb1a2fa41d1c66e48f8f450315f1de62ec616e72
Дмитрий Авилов <diavilov@sbercloud.ru>
2021-03-31 17:28:38 +0300

    storybook confguration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,37 @@
+const path = require('path');
+
 module.exports = {
   "stories": [
-    "../src/stories/**/*.stories.mdx",
-    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
-    "@storybook/preset-scss",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-  ]
+    "@storybook/preset-scss",
+  ],
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'components': path.resolve(__dirname, '../src/components/'),
+    }
+
+    config.module.rules = config.module.rules.map(rule => {
+      if (rule.test.toString().includes('svg')) {
+        const test = rule.test.toString().replace('svg|', '').replace(/\//g, '')
+        return { ...rule, test: new RegExp(test) }
+      } else {
+        return rule
+      }
+    })
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{
+        loader: '@svgr/webpack',
+      }],
+    });
+
+    return config;
+  },
 }
