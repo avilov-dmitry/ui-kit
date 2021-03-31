cd5e6e75d85ce4660b70b34378b579a81fe6ff82
Дмитрий Авилов <diavilov@sbercloud.ru>
2021-03-31 17:29:00 +0300

    stories

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
-
-import { Button, ButtonChangeParamsType, ButtonPropsType } from '../components';
+import { Button, ButtonClickParamsType, ButtonPropsType } from 'components';
 
 export default {
     title: 'Example/Button',
@@ -10,26 +9,29 @@ export default {
     component: Button,
     argTypes: {
         isDisabled: {
-            control: { type: 'boolean' }
+            control: { type: 'boolean' },
+        },
+        isFullWidth: {
+            control: { type: 'boolean' },
         },
     },
     args: {
         text: 'Button',
-        onClick: ({ id }: ButtonChangeParamsType) => console.log('Clicked button with id - ', id),
+        onClick: ({ id }: ButtonClickParamsType) => console.log('Clicked button with id - ', id),
     },
 } as Meta;
 
-const Template: Story<ButtonPropsType> = (args: ButtonPropsType) => <Button {...args} />;
+const Template: Story<ButtonPropsType> = (args) => <Button {...args} />;
 export const SimpleButton = Template.bind({});
 
 
-const WithChildrenTemplate: Story<ButtonPropsType> = (args: ButtonPropsType) => {
+const WithChildrenTemplate: Story<ButtonPropsType> = (args) => {
     const styles = { display: 'inline-block', 'fontSize': '16px', 'lineHeight': '24px' };
 
     return (
         <Button {...args} >
             <div>
-                <span style={{ ...styles, color: '#333',  textDecoration: 'line-through' }}>99 ₽</span>
+                <span style={{ ...styles, color: '#333', textDecoration: 'line-through' }}>99 ₽</span>
                 <span style={{ ...styles, color: args.variant === 'primary' ? '#fff' : '#0083FF', paddingLeft: '20px' }}>79 ₽ / мес</span>
             </div>
         </Button>
@@ -39,7 +41,7 @@ export const PrimaryWithChildren = WithChildrenTemplate.bind({});
 
 const Icon = () => {
     const styles = { display: 'flex', alignItems: 'center', justifyContent: 'center', 'width': '30px', 'height': '100%', fontSize: '16px', lineHeight: '24px' };
-    return  <span style={styles}>❤</span>
-}
-export const WithLeftIcon: Story<ButtonPropsType> = (args: ButtonPropsType) => <Button {...args} leftIcon={Icon} />
-export const WithRightIcon: Story<ButtonPropsType> = (args: ButtonPropsType) => <Button {...args} rightIcon={Icon} />
+    return <span style={styles}>❤</span>;
+};
+export const WithLeftIcon: Story<ButtonPropsType> = (args) => <Button {...args} leftIcon={Icon} />;
+export const WithRightIcon: Story<ButtonPropsType> = (args) => <Button {...args} rightIcon={Icon} />;
--- src/stories/Portal.stories.tsx
+++ src/stories/Portal.stories.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
-
-import { Portal, PortalPropsType } from '../components';
+import { Portal, PortalPropsType } from 'components';
 
 export default {
     title: 'Example/Portal',
